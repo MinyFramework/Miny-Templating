97f38536310c9e97fa51bdbcef240a9c6d045298
Dániel Buga <bugadani@gmail.com>
2014-06-18 17:23:44 +0200

    Restore accidentally deleted file.

---
+++ src/iTemplatingController.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * This file is part of the Minty templating library.
+ * (c) Dániel Buga <bugadani@gmail.com>
+ *
+ * For licensing information see the LICENSE file.
+ */
+
+namespace Modules\Templating;
+
+interface iTemplatingController
+{
+    public function initLayouts();
+}
